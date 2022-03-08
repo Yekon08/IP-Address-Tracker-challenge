c74756c1f8976923a6f6916cf35802360a8614bd
maximeJeannette <maxime.jeannette@akt.io>
2022-03-08 17:33:30 +0100

    misc: setup design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import "./main.css";
+
+import HeaderBg from "./images/pattern-bg.png";
+import ArrowForm from "./images/icon-arrow.svg";
 
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <header style={{ backgroundImage: `url(${HeaderBg})` }}>
+        <h1>IP Adress Tracker</h1>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            placeholder="Search for any IP address or domain"
+          />
+          <button type="submit">
+            <img src={ArrowForm} alt="arrow icon" />
+          </button>
+        </form>
+        <div className="content">Text content</div>
+      </header>
+
+      <main>
+        <div className="map">Map</div>
+      </main>
     </>
   );
 }
